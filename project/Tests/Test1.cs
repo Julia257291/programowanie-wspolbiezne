06f0ee7273dffbb9a74b9b6a09f39ed518e33b45
Julia Szcześniak <257291@edu.p.lodz.pl>
2026-03-21 13:35:59 +0100

    Dodanie testów LogicApi - sprawdzenie czy poprawnie generowane są kuly i czy poprawnie generowany jest promień

--- project/Tests/Test1.cs
+++ project/Tests/Test1.cs
@@ -1,11 +1,26 @@
-﻿namespace Tests
+﻿using Logic;
+
+namespace Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class LogicApiTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateBalls_AmountOfBalls() //Sprawdzamy czy poprawnie generowana jest liczba kul
         {
+            var logic = new LogicApi();
+            int expectedNum = 5;
+            var result = logic.CreateBalls(expectedNum);
+            Assert.IsNotNull(result); // czy lista nie jest pusta
+            Assert.AreEqual(expectedNum, result.Count); //czy stworzyło 5 kul
+        }
+        [TestMethod]
+        public void CreateBalls_CorrectRadius() //Sprawdzamy czy promień kul jest poprawny
+        {
+            var logic = new LogicApi();
+            var result = logic.CreateBalls(1);
+            Assert.IsTrue(result.Count > 0);
+            Assert.AreEqual(10, result[0].Radius); // Sprawdzamy promień pierwszej kuli na liście
         }
     }
 }
