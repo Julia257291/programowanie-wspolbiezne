ec07f9e96deeb5baab6266d35457d6f7f94478dd
Julia Szcześniak <257291@edu.p.lodz.pl>
2026-03-21 13:20:32 +0100

    Struktura warstwowa projektu - Data, Logic, Presentation i Tests. Implementacja klasy Ball w Data oraz LogicApi w Logic

---
+++ project/Logic/LogicApi.cs
@@ -0,0 +1,17 @@
+﻿using Data;
+
+namespace Logic
+{
+    public class LogicApi
+    {
+        public List<Ball> CreateBalls(int count)
+        {
+            var balls = new List<Ball>();
+            for (int i = 0; i < count; i++)
+            {
+                balls.Add(new Ball { X = i * 15, Y = i * 15, Radius = 10 });
+            }
+            return balls;
+        }
+    }
+}
---
+++ project/project/Ball.cs
@@ -0,0 +1,9 @@
+﻿namespace Data
+{
+    public class Ball
+    {
+        public double X { get; set; } //oś X
+        public double Y { get; set; } //oś Y
+        public double Radius { get; set; } //promień - wielkość
+    }
+}
