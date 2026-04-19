e9257a297afab8fcf2c2216bfd191157cd303e8a
Julia Szcześniak <257291@edu.p.lodz.pl>
2026-04-19 16:20:54 +0200

    klasa DataApi z losowym przydziałem miejsc dla kul

---
+++ project/project/DataAbstractApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public abstract class DataAbstractApi
+    {
+        //musi umieć stworzyć kule, maxX i maxY to ograniczenia dla pozycji kul, żeby nie wychodziły poza obszar
+        public abstract void CreateBalls(int count, double maxX, double maxY);
+        public abstract List<Ball> GetBalls(); //metoda do pobierania kul w logice, żeby można było je potem wyświetlić
+
+        public static DataAbstractApi CreateApi()
+        {
+            return new DataApi();
+        }
+    }
+}
---
+++ project/project/DataApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    internal class DataApi : DataAbstractApi
+    {
+        private readonly List<Ball> _balls = new List<Ball>();
+        private readonly Random _random = new Random();
+
+        public override void CreateBalls(int count, double maxX, double maxY)
+        {
+            _balls.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                double radius = 10.0;
+                var ball = new Ball
+                {
+                    X = _random.NextDouble() * (maxX - radius), //NextDouble() zwraca wartość z zakresu [0.0, 1.0)
+                    Y = _random.NextDouble() * (maxY-radius ),
+                    Radius = radius
+                };
+                _balls.Add(ball);
+            }
+        }
+
+        public override List<Ball> GetBalls()
+        {
+            return _balls;
+        }
+    }
+}
