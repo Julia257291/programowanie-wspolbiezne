946e880f110b860ae48f1512c014dead45b1b83e
Julia Szcześniak <257291@edu.p.lodz.pl>
2026-04-19 15:51:48 +0200

    implementacja kuli z INotifyPropertyChanged

--- project/project/Ball.cs
+++ project/project/Ball.cs
@@ -1,9 +1,26 @@
-﻿namespace Data
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
 {
-    public class Ball
+    public class Ball : INotifyPropertyChanged
     {
-        public double X { get; set; } //oś X
-        public double Y { get; set; } //oś Y
+        private double _x; private double _y;
+
+        //Kiedy x zostaje zmieniony, wywołujemy OnPropertyChanged, aby powiadomić UI o zmianie
+        public double X { get { return _x; }
+            set { _x = value;  OnPropertyChanged(); } 
+        } //oś X
+        public double Y { get { return _y; }
+            set { _y = value; OnPropertyChanged(); } 
+        } //oś Y
         public double Radius { get; set; } //promień - wielkość
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
